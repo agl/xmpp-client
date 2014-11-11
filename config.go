25d402a660425de82f363320789baa8af75f3d31
Filippo Valsorda <filippo@cloudflare.com>
2014-11-11 17:10:16 +0000

    s|code.google.com/p/go.|golang.org/x|

--- config.go
+++ config.go
@@ -11,10 +11,10 @@ import (
 	"strconv"
 	"strings"
 
-	"code.google.com/p/go.crypto/otr"
-	"code.google.com/p/go.crypto/ssh/terminal"
-	"code.google.com/p/go.net/proxy"
 	"github.com/agl/xmpp"
+	"golang.org/x/crypto/otr"
+	"golang.org/x/crypto/ssh/terminal"
+	"golang.org/x/net/proxy"
 )
 
 type Config struct {
--- ui.go
+++ ui.go
@@ -20,11 +20,11 @@ import (
 	"syscall"
 	"time"
 
-	"code.google.com/p/go.crypto/otr"
-	"code.google.com/p/go.crypto/ssh/terminal"
-	"code.google.com/p/go.net/html"
-	"code.google.com/p/go.net/proxy"
 	"github.com/agl/xmpp"
+	"golang.org/x/crypto/otr"
+	"golang.org/x/crypto/ssh/terminal"
+	"golang.org/x/net/html"
+	"golang.org/x/net/proxy"
 )
 
 var configFile *string = flag.String("config-file", "", "Location of the config file")
